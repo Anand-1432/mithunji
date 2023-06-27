19ad0010907580a227d6598a1c3fbd7966dc34ad
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2023-06-27 10:37:33 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/home/Home"
+import Login from "./pages/login/Login"
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   )
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,40 @@ import "./home.scss"
 const Home = () => {
     return (
         <>
-            <div className="home">Mithuji Dev</div>
+            <div className="home">
+
+                <nav> mithuji </nav>
+
+                <div className="topContainer">
+                    <div className="topInner">
+                        <div className="options">
+                            <div>Product</div>
+                            <div>Category</div>
+                            <div>Seller</div>
+                        </div>
+
+                        <div className="searchSection">
+                            <input type="text" className="form-control" placeholder="Search for Anything..." />
+                            <button className="btn">sds</button>
+                        </div>
+
+                        <div className="selectCon">
+                            <select name="" id="">
+                                <option value="">All India</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="contentSection"></div>
+
+                <div className="midContainer"></div>
+
+                <div className="productCategoryCon"></div>
+                <div className="productCategoryCon"></div>
+                <div className="productCategoryCon"></div>
+
+            </div>
         </>
     )
 }
